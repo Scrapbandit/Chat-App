18d1190795241d425a63d9bf72e8197ec4fab90e
Scrapbandit <99894181+Scrapbandit@users.noreply.github.com>
2022-07-25 12:10:10 +0200

    add default to chat export
     screen showing up but empty

--- components/Chat.js
+++ components/Chat.js
@@ -1,29 +1,3 @@
-// import React from 'react';
-
-// import { View, Text, Button, TextInput, StyleSheet } from 'react-native';
-
-// export default class Chat extends React.Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = { name: "" };
-//       }
-
-//   render() {
-//     let name = this.props.route.params.name;
-//     this.props.navigation.setOptions ({ title: name });
-
-//     return (
-//         <View style={{flex:1, justifyContent: 'center', alignItems: 'center'}}>
-//         <Text>Chat here</Text>
-//         <Button
-//           title="Go to Start"
-//           onPress={() => this.props.navigation.navigate('Start')}
-//         />
-//       </View>
-//     );
-//   };
-// }
-
 import React from "react";
 import { GiftedChat } from "react-native-gifted-chat";
 import {
@@ -36,7 +10,7 @@ import {
   KeyboardAvoidingView,
 } from "react-native";
 
-export class Chat extends React.Component {
+export default class Chat extends React.Component {
   constructor() {
     super();
     this.state = {
@@ -72,7 +46,7 @@ export class Chat extends React.Component {
       messages: GiftedChat.append(previousState.messages, messages),
     }));
   }
-  
+
   renderBubble(props) {
     return (
       <Bubble
